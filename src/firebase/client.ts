d4c8768d0d794d59dfec0efde379136e9cb0ef87
luiggy2620 <luiggymamani@gmail.com>
2026-03-31 22:04:42 -0400

    feat: added border as website type of image for service main image

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+// 1. Importar getAnalytics e isSupported
+import { getAnalytics, isSupported, type Analytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: import.meta.env.PUBLIC_APIKEY,
@@ -14,3 +16,15 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+
+export let analytics: Analytics | undefined;
+
+if (typeof window !== "undefined") {
+  isSupported().then((supported) => {
+    if (supported) {
+      console.log("Initializing analytics");
+      analytics = getAnalytics(app);
+      console.log("Initialized");
+    }
+  });
+}
