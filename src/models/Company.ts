17d3afac1bd203af369cd54da2a5283877c95582
luiggy2620 <luiggymamani@gmail.com>
2026-04-21 12:04:27 -0400

    feat: added videos to the hero section

--- src/models/Company.ts
+++ src/models/Company.ts
@@ -1,6 +1,24 @@
 export interface Company {
   name: string;
+  slogan: string;
+  shortDescription: string;
+  logos: Logos;
+  whyToUse: ReasonToUse[];
+  videos: string[];
   socialMedia: SocialMedum[];
+}
+
+export interface ReasonToUse {
+  reason: string;
+  icon: string;
+  description: string;
+}
+
+export interface Logos {
+  icon: string;
+  iconTitle: string;
+  iconTitleRow: string;
+  title: string;
 }
 
 export interface SocialMedum {
