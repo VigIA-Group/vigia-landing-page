652960f271a40f29f6eb954dd8dcc6075125dee9
luiggy2620 <luiggymamani@gmail.com>
2026-06-29 23:45:32 -0400

    feat: added vigia title to the navbar

--- src/components/navigation/constants/NavLinks.ts
+++ src/components/navigation/constants/NavLinks.ts
@@ -11,15 +11,19 @@ export const NAV_LINKS = [
     href: LANDING_ROUTES.services,
   },
   {
-    name: "¿Por qué nosotros?",
+    name: "Precios",
+    href: PRICE_ROUTES.calculator,
+  },
+  {
+    name: "Logo",
     href: LANDING_ROUTES.whyToUse,
   },
   {
-    name: "Contacto",
-    href: LANDING_ROUTES.footer,
+    name: "¿Por qué VigIA?",
+    href: LANDING_ROUTES.whyToUse,
   },
   {
-    name: "Precios",
-    href: PRICE_ROUTES.calculator,
+    name: "Nosotros",
+    href: LANDING_ROUTES.footer,
   },
 ];
